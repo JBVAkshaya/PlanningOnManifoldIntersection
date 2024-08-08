0ec48c1e880c90e913b8bc550e122f5da17831a0
JBVAkshaya <agrawaak@oregonstate.edu>
2024-08-07 18:29:31 -0700

    robot configurations

---
+++ experiment_configs/I_config.py
@@ -0,0 +1,72 @@
+num_robots = 5
+struct_type = 'I'
+rob_specs = {
+    'b_x':0.6,
+    'b_y':0.6,
+    'b_z':0.6,
+    'l_1':0.3,
+    'l_2':0.1,
+    'base_z':0.0, # Update this
+    'type':'UVMS', # can be UVMS, TB3OM
+    'dof': ['x', 'y', 'z', 'yaw', 'q1', 'q2']
+
+}
+
+robot_params = {
+    'num_bots':num_robots,
+    'struct_type': struct_type,
+    'base_z':0.0,
+    'dof': ['x', 'y', 'z', 'yaw', 'q1', 'q2'],
+    'min_len':0.5
+}
+
+constraint_params = {
+    'm1': {
+            'min_len': 0.5,
+            'struct_type': struct_type,
+            'num_robots': num_robots,
+            'penalty_factor': 3.0,
+            'eps':0.3
+
+        },
+    'm2': {
+            'min_len': 0.5,
+            'struct_type': struct_type,
+            'num_robots': num_robots,
+            'penalty_factor': 1.4,
+            'eps':0.3
+        },
+    'm3': {
+            'min_len': 0.5,
+            'struct_type': struct_type,
+            'num_robots': num_robots,
+            'penalty_factor': 3.0,
+            'eps':0.3
+        },
+    'm4': {
+            'min_len': 0.5,
+            'struct_type': struct_type,
+            'num_robots': num_robots,
+            'penalty_factor': 3.0,
+            'eps':0.3
+        }
+    }
+
+rrt_params = {
+    'n_samples' : 120,
+    'gamma': 2.0,
+    'alpha': 7.0 ,##### Right now direct radius to find nearest neighbor
+    'beta': 0.5, # prb of sampling goal (Generally 0.1)
+    'collision_res': 0.1,
+    'proj_eps': 0.3, #### This will change as number of robots will increase
+    'proj_max_iter': 1000,
+    'converge_thresh':7.0,
+    'step_factor': 2.5
+} 
+
+plot_params = {
+    'debug_collision':False,
+    'obs_3d':True, 
+    'struct_type': struct_type
+}
+debug_flag = False
---
+++ experiment_configs/three_S_config.py
@@ -0,0 +1,73 @@
+#! /usr/bin/python3
+
+num_robots =  3
+struct_type = 'S'
+rob_specs = {
+    'b_x':0.6,
+    'b_y':0.6,
+    'b_z':0.6,
+    'l_1':0.3,
+    'l_2':0.1,
+    'base_z':0.0, # Update this
+    'type':'UVMS', # can be UVMS, TB3OM
+    'dof': ['x', 'y', 'z', 'yaw', 'q1', 'q2']
+
+}
+
+robot_params = {
+    'num_bots':num_robots,
+    'struct_type': struct_type,
+    'base_z':0.0,
+    'dof': ['x', 'y', 'z', 'yaw', 'q1', 'q2'],
+    'min_len':0.5
+}
+
+constraint_params = {
+    'm1': {
+            'min_len': 0.5,
+            'struct_type': struct_type,
+            'num_robots': num_robots,
+            'penalty_factor': 1.0,
+            'eps': 0.3
+        },
+    'm2': {
+            'min_len': 0.5,
+            'struct_type': struct_type,
+            'num_robots': num_robots,
+            'penalty_factor': 1.0,
+            'eps': 0.3
+        },
+    'm3': {
+            'min_len': 0.5,
+            'struct_type': struct_type,
+            'num_robots': num_robots,
+            'penalty_factor': 0.5,
+            'eps': 0.3
+        },
+    'm4': {
+            'min_len': 0.5,
+            'struct_type': struct_type,
+            'num_robots': num_robots,
+            'penalty_factor': 1.0,
+            'eps': 0.3
+        }
+    }
+
+rrt_params = {
+    'n_samples' : 100,
+    'gamma': 2.0, # Not used: for reference check in rrtstar_manifold.py
+    'alpha': 7.0 ,##### Right now direct radius to find nearest neighbor
+    'beta': 0.3, # prb of sampling goal (Generally 0.1)
+    'collision_res': 0.1,
+    'proj_eps': 0.1, #### This will change as number of robots will increase
+    'proj_max_iter': 200,
+    'converge_thresh':6.0,
+    'step_factor': 2.2 # Step size for generating new sample in direction d
+} 
+
+plot_params = {
+    'debug_collision':False,
+    'obs_3d':True, 
+    'struct_type': struct_type
+}
+debug_flag = True
